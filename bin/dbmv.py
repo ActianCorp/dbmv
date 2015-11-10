df110799916edfcbf5010968b1958e5eddf8d316
OlivierMeignan <olivier.meignan@actian.com>
2015-11-10 11:58:25 +0100

    Add copyright info

--- bin/dbmv.py
+++ bin/dbmv.py
@@ -2,10 +2,24 @@
 # -*- coding: utf-8 -*-
 # For UTF-8 see: http://www.python.org/dev/peps/pep-0263/ 
 # PYTHONIOENCODING=utf_8
-# Ajouter traitement des erreurs de parametres (--par=)
-# Ajouter parametre extract_data_to_file
+
+# Copyright 2015 Actian Corporation
+ 
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+ 
+#      http://www.apache.org/licenses/LICENSE-2.0
+ 
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+
+# Add:
 ## Add test on not easily convertable datatypes (MSSQL : sql_variant, xml, byte(s), image, ...)
-## Voir comment se comporte 
 
 import sys, os, traceback
 import getopt, re                 # Import basic modules
